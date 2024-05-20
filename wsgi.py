7998ed8e426c190df67d36e088ea99f64441f9d2
Julscha9291 <j.schaepermeier91@gmail.com>
2024-05-20 14:55:45 +0200

    update settings

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,10 @@ import django
 from django.core.wsgi import get_wsgi_application
 import sys
 
-sys.path.append('/home/pi/party') 
+sys.path.append('/home/pi/birthday') 
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'party.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthday.settings')
 
 application = get_wsgi_application()
 
